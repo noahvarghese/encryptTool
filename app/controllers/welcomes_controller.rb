9e07e9ec1419e7dfda6e5ba39445967274833106
noahvarghese <varghese.noah@gmail.com>
2020-05-18 10:32:26 -0400

    added base view

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,10 @@
+class WelcomesController < ApplicationController
+  before_action :set_welcome, only: [:show, :edit, :update, :destroy]
+
+  # GET /welcomes
+  # GET /welcomes.json
+  def index
+    @welcomes = Welcome.all
+    render 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/decrypt', to: 'decrypt#decrypt' 
 
-  post 'encrypt', to: 'encrypt#encrypt'
+  post '/encrypt', to: 'encrypt#encrypt'
+
+  get '/', to: 'welcomes#index'
 end
