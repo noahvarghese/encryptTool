9e4773250deda595ec54168227245325d575ecfb
noahvarghese <varghese.noah@gmail.com>
2020-05-25 18:05:25 -0400

    added relative_url_root for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_controller.relative_url_root = "/cryptoTool"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
